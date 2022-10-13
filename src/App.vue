151d04cac858f10e5fef87459a374f5d3caf881a
AdonayGarciaTrujillo <a3521110522@alumno.uttehuacan.edu.mx>
2022-10-12 20:57:13 -0500

    Examen AGT 4°A

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,33 @@
 <template>
-  <header class="text-center text-white" style="background-color: black; width: 200%; text-align: center;">
+  <header class="text-center text-white" style="background-color: orange; width: 100%; height: 65px; text-align: center;">
 		
 	
-		<h1>Very-Deli Adonay Garcia Trujillo 4A</h1>
+		<h1>Very-Deli Adonay Garcia Trujillo 4°A</h1>
 			
 
 	</header>
-
+  
   <br>
-  <h3 style="text-align: center; width: 200%;">Comida rapida</h3>
+  <h3 style="text-align: center; width: 100%;">Comida rapida</h3>
  
     <br>
   <card nombre="Hamburguesa Doble" descripsion="Hamburguesa con doble carne, doble queso, vegetales, condimentos y salsa casera"  precio="$130.00 MXN"  > <img src="https://th.bing.com/th/id/R.ee7e8446d67df0a1c88ced2e9d121777?rik=DvzoV7Zf7G%2bKPA&riu=http%3a%2f%2frapiandres.com%2fwp-content%2fuploads%2f2020%2f04%2fhamburguesadoble.jpg&ehk=aoTiQgsqsLrDEHcxjEMbnkEh9aAA1Xd%2foLjlnPhVAfw%3d&risl=&pid=ImgRaw&r=0" height="200" alt=""></card> 
   <card nombre="Pizza Pepperoni" descripsion="Pizza de pepperoni y extra queso, con salsa de tomate y masa fresca" precio="$200.00 MXN." > <img src="https://th.bing.com/th/id/OIP.fWErzWmVzSW64PN_UoGCXgHaJF?pid=ImgDet&rs=1" height="200" alt=""> </card>
-  <card nombre="Papas a la Francesa"  descripsion="Papas a la francesa sazonadas con Lemmon Pepper, acompañadas de salsa catsup" precio="$70.00 MXN." > <img src="https://th.bing.com/th/id/R.67681f438e22ed47364dc6490620ab00?rik=HV%2fGRAbxTJNhzA&pid=ImgRaw&r=0&sres=1&sresct=1" height="200" width="300" alt=""> </card>
+  <card nombre="Papas a la Francesa"  descripsion="Papas a la francesa sazonadas con Lemmon Pepper, acompañadas de salsa catsup" precio="$70.00 MXN." > <img src="https://th.bing.com/th/id/R.67681f438e22ed47364dc6490620ab00?rik=HV%2fGRAbxTJNhzA&pid=ImgRaw&r=0&sres=1&sresct=1" height="200"  alt=""> </card>
   <card nombre="Helado Napolitano"  descripsion="Rollitos de helado recien hechos con chocolate blanco y chocolate negro derretido"  precio="$110.00 MXN." > <img src="https://th.bing.com/th/id/R.1128b7dfc6a9a1960f9a7606ccd02324?rik=VtfpwHMSnnBp%2bA&pid=ImgRaw&r=0" height="200" alt=""> </card>
-  <card nombre="Hot Dog Argen" descripsion="Hot dog argentino con pan recien horneado, salchicha argentina, picadillo de tomate y cebolla, acompañado de salsa catsup, mostaza y mayonesa" precio="$90.00 MXN." > <img src="https://www.gannett-cdn.com/presto/2020/08/19/PPHX/22b683f8-8b99-4485-96c9-8638f09a5bab-Caprichoso_-_Dogs.jpg?crop=4467,2513,x0,y474&width=3200&height=1801&format=pjpg&auto=webp" height="200" width="300" alt=""> </card>
+  <card nombre="Hot Dog Argen" descripsion="Hot dog argentino con pan recien horneado, salchicha argentina, picadillo de tomate y cebolla, acompañado de salsa catsup, mostaza y mayonesa" precio="$90.00 MXN." > <img src="https://www.gannett-cdn.com/presto/2020/08/19/PPHX/22b683f8-8b99-4485-96c9-8638f09a5bab-Caprichoso_-_Dogs.jpg?crop=4467,2513,x0,y474&width=3200&height=1801&format=pjpg&auto=webp" height="160"  alt=""> </card>
   <card nombre="Lasagna" descripsion="Lasaña de carne molida con 5 capas de pasta, queso, salsa de tomate y condimentos" precio="$150.00 MXN."> <img src="https://th.bing.com/th/id/R.d20441ef273c07aa60be1752dc6ceb06?rik=8UzheYhZTA2d6g&pid=ImgRaw&r=0" height="200" width="300" alt=""></card>
   
- 
+  <footer class="text-center" style="background-color: orange; width: 100%; text-align: center; height: 50px; width: 100%;">
   
-  <footer class="text-center text-white" style="background-color: black; width: 200%; text-align: center; height: 50px; width: 200%;">
-   
-  
-    
-    
-  
-  
-    <span class="copyright" style="text-align: center;">&copy;2022, UTTehuacan, All rights reserved¿.</span>
+    <span class="copyright" style="text-align: center;">&copy;2022, UTTehuacan, All rights reserved.</span>
  
   </footer>
 
+
 </template>
 
-
+2022, UTTehuacan, All rights reserved¿.
 
 <script>
     import Card from "./components/Card.vue"
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,34 +7,30 @@
                 <p class="text-center"> {{ nombre }} </p>
                 <p class="text-center"> {{ descripsion }} </p>
                 <p class="text-center"> {{ precio }} </p>
-                <a href="https://bilbao.edu.mx/wp-content/uploads/2021/10/comida_rapida_1.jpg" target="_blank">Comprar</a>
+                <a href="https://www.facebook.com/" target="_blank">Comprar</a>
     </div>
     
   
 </template>   
 
+
+
+
 <style scoped>
    
     
     div{
-    
-       
+        width: 300px;
         display: inline-block; 
         text-align: center;
-        border: 0.5px solid #f2f2f2;
-        border-radius: 50px;
-        margin:  1%;
+        border: 1px solid black;
+        border-radius: 5px;
+        margin: 2%;
         padding: 1%;
+      
 
     }
-    header {
-        display: flex;
-        width: 10%;
-        size: 100%;
-        height: 10%;
-        font-size: 12px;
-        
-    }
+   
     
    
 </style>
